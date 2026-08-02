7662e0e4f22293684cfd1b22846744b9e98d5a0e
21Z121Z1 <89170834+21Z121Z1@users.noreply.github.com>
2026-08-02 16:48:07 +0800

    iris: remove unused hazard graph import

--- src/main/java/com/metallum/client/metal/render/IrisMetalHazardGraph.java
+++ src/main/java/com/metallum/client/metal/render/IrisMetalHazardGraph.java
@@ -1,7 +1,6 @@
 package com.metallum.client.metal.render;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
