b02af1edc2326121a647450b8f719d63e62e54b8
21Z121Z1 <89170834+21Z121Z1@users.noreply.github.com>
2026-09-21 07:21:43 +0800

    Isolate gameplay mixin package from workload classes

--- .github/ci/minecraft-e2e/src/main/java/com/metallum/e2e/mixin/GameplayFrameMixin.java
+++ .github/ci/minecraft-e2e/src/main/java/com/metallum/e2e/mixin/GameplayFrameMixin.java
@@ -1,5 +1,6 @@
-package com.metallum.e2e;
+package com.metallum.e2e.mixin;
 
+import com.metallum.e2e.VanillaGameplay;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
